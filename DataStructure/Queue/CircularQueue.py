4e4f3e1e06c200c5ee35988e57761b43ba352225
JJong <59076451+dldnxks12@users.noreply.github.com>
2021-07-14 17:55:08 +0900

    Update CircularQueue.py

--- DataStructure/Queue/CircularQueue.py
+++ DataStructure/Queue/CircularQueue.py
@@ -9,3 +9,67 @@ front , rear 초기값 0
   --> front == (rear+1)%(size) -> Queue is Full
 
 '''
+
+
+def IsQueueFull():
+  global front, rear, size, Queue
+
+  if front == (rear+1)%size :
+    return True
+  else:
+    return False
+
+def IsQueueEmpty():
+  global front, rear, size, Queue
+
+  if front == rear  :
+    return True
+  else:
+    return False
+  
+
+def enQueue(data):
+  global front, rear, size, Queue  
+  if IsQueueFull():
+    return
+  else:
+    rear = (rear+1)%size
+    Queue[rear] = data
+    
+def deQueue():
+  global front, rear, size, Queue  
+  if IsQueueEmpty():
+    return 
+  else:
+    front = (front+1)%size
+    data = Queue[front]
+    Queue[front] = None
+    return data 
+
+size  = 10
+Queue = [ None for _ in range(size)]
+front = 0
+rear  = 0
+
+
+if __name__ == "__main__":
+
+  enQueue("종수1")
+  enQueue("종수2")
+  enQueue("종수3")
+  enQueue("종수4")
+  enQueue("종수5")
+  enQueue("종수6")
+  enQueue("종수7")
+  enQueue("종수8")
+  enQueue("종수9")
+  data = deQueue()
+  data = deQueue()
+  enQueue("종수10")
+  enQueue("종수11")
+  data = deQueue()
+  data = deQueue()
+  data = deQueue()
+  data = deQueue()  
+
+  print(Queue)
